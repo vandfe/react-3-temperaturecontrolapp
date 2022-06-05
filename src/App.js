e02b44ad97d8dd15bfb9a5ca31e292b505902ce6
Félix Vandal <37978866+vandfe@users.noreply.github.com>
2022-06-05 00:07:20 -0400

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [temp, setTemp] = useState(10)
+  const [tempColor, setTempColor] = useState('cold')
+
+  const upTemp = () => {
+    if(temp < 30)
+      setTemp(temp+1)
+    //changeColorDisplay()
+  }
+
+  const downTemp = () => {
+    if(temp > 0)
+      setTemp(temp-1)
+    //changeColorDisplay()
+  }
+
+  useEffect(() =>{
+    if(temp < 15)
+      setTempColor('cold')
+    else if(temp > 20)
+      setTempColor('hot')
+    else
+      setTempColor('neutral')
+  },[temp])
+  
+
+  /*
+  const changeColorDisplay = () => {
+    if(temp < 15)
+      setTempColor('cold')
+    else if(temp > 20)
+      setTempColor('hot')
+    else
+      setTempColor('neutral')
+  }
+  */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <div className='temperature-display-container'>
+        <div className={`temperature-display ${tempColor}`} >
+          {temp} C
+        </div>
+      </div>
+      <div className='button-container'>
+          <button onClick={() => upTemp()}>+</button>
+          <button onClick={() => downTemp()}>-</button>
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
